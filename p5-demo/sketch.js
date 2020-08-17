4a73985f3705fa632439618d0a91716e281929db
ukpfilms <jack_walnut@hotmail.com>
2020-08-16 23:32:31 -0700

    jack first commit

--- p5-demo/sketch.js
+++ p5-demo/sketch.js
@@ -1,19 +1,12 @@
 function setup() {
   // put setup code here
-  createCanvas(500, 500);
+  createCanvas(500,500);
   background(100);
 }
 
 function draw() {
   // put drawing code here
-  background(200);
-
-  noStroke();
-  ellipse(50, 50, 100);
-
-  stroke('red');
-  ellipse(200, 110, 100);
-
-  
-  ellipse(mouseX, mouseY, 100);
+  background(100);
+ellipse(50,50,100,100)
+rect (mouseX,mouseY,50,50)
 }
